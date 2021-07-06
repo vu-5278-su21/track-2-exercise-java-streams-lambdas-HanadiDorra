d9c1a10fe6a91a6c21837d1dc5135a5f87d6f297
Hanadi Dorra <hanadi.dorra@vanderbilt.edu>
2021-07-06 12:59:36 -0400

    Update BikeRide.java

--- src/main/java/edu/vanderbilt/cs/streams/BikeRide.java
+++ src/main/java/edu/vanderbilt/cs/streams/BikeRide.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class BikeRide {
+public class BikeRide {//
 
     public static class DataFrame {
         public final double velocity;
